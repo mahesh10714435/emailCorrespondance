a813c01e1f14a69c27b4cc7acfb39ea2e4fb3817
Mahesh Pagare <mahesh.10714435@ltimindtree.com>
2024-07-30 11:48:33 +0530

    added left right section

--- src/component/EmailCorrespondence.tsx
+++ src/component/EmailCorrespondence.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Grid from '@mui/material/Grid';
   export default function EmailCorrespondence() {
     return (
-      <div>
-        Email Currespondence screeen
-      </div>
+        <Grid container spacing={2}>
+        <Grid item xs={4}>
+         <div style={{height:'500px',border:'1px solid'}}>sdsd</div>
+        </Grid>
+        <Grid item xs={8}>
+        <div style={{height:'500px',border:'1px solid'}}>sdsd</div>
+        </Grid>
+       </Grid> 
     );
   }
